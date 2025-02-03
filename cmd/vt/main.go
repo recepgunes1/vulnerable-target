15534228f889d50be2cda04a2028d52a8d87058b
Recep Gunes <recepgunes2@outlook.com>
2025-02-03 22:41:51 +0300

    migrate cobra

--- cmd/vt/main.go
+++ cmd/vt/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/happyhackingspace/vulnerable-target/internal/cli"
 )
 
 func main() {
-	app := cli.NewApp()
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
+	cli.Execute()
 }
